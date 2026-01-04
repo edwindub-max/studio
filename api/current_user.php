4cd29ad2fdf16bf71757a4a464d91c541488a09a
edwindub-max <edwindubarry32@gmail.com>
2026-01-04 20:56:54 +0100

    Delete api directory

--- api/current_user.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__.'/session_manager.php';
-echo json_encode(['user'=>($_SESSION['user'] ?? null)]);
--- api/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-session_start();
-$_SESSION = [];
-session_destroy();
-echo json_encode(['success'=>true]);
